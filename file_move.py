03ab3844808e5e1cff951eb95b62b9532b39f4a5
Tatsuoki, Takayuki <takayuki.tatsuoki@gmail.com>
2020-04-16 18:43:49 +0900

    Added one development tool.
    Signed-off-by: Tatsuoki, Takayuki <takayuki.tatsuoki@gmail.com>

---
+++ file_move.py
@@ -0,0 +1,14 @@
+import shutil
+from pathlib import Path
+
+p = Path('/mnt/chromeos/MyFiles/Downloads')
+js = Path('/home/takayukitatsuoki/oed-dev/public')
+res = Path('/home/takayukitatsuoki/oed-dev/public/res')
+for x in p.iterdir():
+    if x.is_file():
+        y = Path(str(js) + '/' + x.name)
+        if x.suffix == '.md':
+            y = Path(str(res) + '/' + x.name)
+        if y.exists():
+            y.unlink()
+        shutil.move(str(x), str(y))
