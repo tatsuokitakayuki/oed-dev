09d51059d030562ebdbbf85debae9ac28e04788b
Tatsuoki, Takayuki <takayuki.tatsuoki@gmail.com>
2020-05-01 14:25:05 +0900

    Updated development tool.
    Signed-off-by: Tatsuoki, Takayuki <takayuki.tatsuoki@gmail.com>

--- build_file_list.py
+++ build_file_list.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+
 from pathlib import Path
 
 oed = Path('/home/takayukitatsuoki/oed-dev/public')
 oed_res = Path('/home/takayukitatsuoki/oed-dev/public/res')
+oed_icons = Path('/home/takayukitatsuoki/oed-dev/public/images/icons')
 ace = Path('/home/takayukitatsuoki/oed-dev/public/ace')
 ace_snippets = Path('/home/takayukitatsuoki/oed-dev/public/ace/snippets')
 
@@ -12,10 +15,13 @@ for x in oed.iterdir():
 for x in oed_res.iterdir():
     if x.is_file():
         items.append('res/' + x.name)
+for x in oed_icons.iterdir():
+    if x.is_file():
+        items.append('images/icons/' + x.name)
 items.sort()
 file = open('./oed_list.txt', 'w');
 for x in items:
-    file.write('\'./' + x + '\'\n')
+    file.write('\'./' + x + '\',\n')
 file.close()
 
 items = []
@@ -28,5 +34,5 @@ for x in ace_snippets.iterdir():
 items.sort()
 file = open('./ace_list.txt', 'w');
 for x in items:
-    file.write('\'./' + x + '\'\n')
+    file.write('\'./' + x + '\',\n')
 file.close()
