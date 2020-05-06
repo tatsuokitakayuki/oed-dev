0c741e5e375665156fd22d9a18c12a49b1923284
Tatsuoki, Takayuki <takayuki.tatsuoki@gmail.com>
2020-05-06 16:36:51 +0900

    Added one development tool.
    Signed-off-by: Tatsuoki, Takayuki <takayuki.tatsuoki@gmail.com>

---
+++ copy_icons.sh
@@ -0,0 +1,9 @@
+convert /mnt/chromeos/MyFiles/Downloads/OED-icon-512x512.png -resize 384x384! /home/takayukitatsuoki/oed-dev/public/images/icons/icon-384x384.png
+convert /mnt/chromeos/MyFiles/Downloads/OED-icon-512x512.png -resize 192x192! /home/takayukitatsuoki/oed-dev/public/images/icons/icon-192x192.png
+convert /mnt/chromeos/MyFiles/Downloads/OED-icon-512x512.png -resize 152x152! /home/takayukitatsuoki/oed-dev/public/images/icons/icon-152x152.png
+convert /mnt/chromeos/MyFiles/Downloads/OED-icon-512x512.png -resize 144x144! /home/takayukitatsuoki/oed-dev/public/images/icons/icon-144x144.png
+convert /mnt/chromeos/MyFiles/Downloads/OED-icon-512x512.png -resize 128x128! /home/takayukitatsuoki/oed-dev/public/images/icons/icon-128x128.png
+convert /mnt/chromeos/MyFiles/Downloads/OED-icon-512x512.png -resize 96x96! /home/takayukitatsuoki/oed-dev/public/images/icons/icon-96x96.png
+convert /mnt/chromeos/MyFiles/Downloads/OED-icon-512x512.png -resize 72x72! /home/takayukitatsuoki/oed-dev/public/images/icons/icon-72x72.png
+cp /mnt/chromeos/MyFiles/Downloads/OED-icon-512x512.png /home/takayukitatsuoki/oed-dev/public/images/icons/icon-512x512.png
+cp /mnt/chromeos/MyFiles/Downloads/OED-icon-512x512.png /home/takayukitatsuoki/oed-dev/public/images/icons/favicon.png
