dd5a6121b30310fa0aab444740fdeb1bbdc065aa
Tatsuoki, Takayuki <takayuki.tatsuoki@gmail.com>
2020-06-09 16:04:32 +0900

    Updated.
    Signed-off-by: Tatsuoki, Takayuki <takayuki.tatsuoki@gmail.com>

--- move_file.py
+++ move_file.py
@@ -3,14 +3,16 @@
 import shutil
 from pathlib import Path
 
-p = Path('/mnt/chromeos/MyFiles/Downloads')
-js = Path('/home/takayukitatsuoki/oed-dev/public')
-res = Path('/home/takayukitatsuoki/oed-dev/public/res')
-for x in p.iterdir():
-    if x.is_file():
-        y = Path(str(js) + '/' + x.name)
-        if x.suffix == '.md':
-            y = Path(str(res) + '/' + x.name)
-        if y.exists():
-            y.unlink()
-        shutil.move(str(x), str(y))
+source_dir = Path('/mnt/chromeos/MyFiles/Downloads')
+oed_dev = Path('/home/takayukitatsuoki/oed-dev')
+public = oed_dev / 'public'
+res = public / 'res'
+for file in source_dir.iterdir():
+    if file.is_file():
+        dest = public / file.name
+        if file.suffix == '.md':
+            dest = res / file.name
+        if dest.exists():
+            dest.unlink()
+        shutil.move(str(file), str(dest))
+        print(dest.name)
